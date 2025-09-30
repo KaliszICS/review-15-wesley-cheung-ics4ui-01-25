baea630facaf70a92c4dd70bb0ed96d4a7cd8fdb
wesley-cheung-ics4ui-01-25 <166552362+wesley-cheung-ics4ui-01-25@users.noreply.github.com>
2025-09-30 18:15:05 +0000

    update practiceproblems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,23 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(goodString("pqr"));
+	}
+	public static int goodString(String input) {
+		int min = 10000;
+		for (int i = 0;i<input.length();i++) {
+			int get = input.charAt(i);
+			for(int j = 0;j<(input.length()-i);j++) {
+				if((input.charAt(input.length()-1-j))==(get) && input.length()-j-i>=2) {
+					min = Math.min(min, i+j);
+				}
+			}
+		}
+		if (min == 10000) {
+			return -1;
+		} else {
+			return min;
+		}
 	}
 
 }
